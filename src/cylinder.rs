e158294f3fc945112080517cc0f3db7fea8c2a70
Leon Suchy <lafares.lae@gmail.com>
2022-04-08 15:16:43 +0200

    Added cylinders without UV mapping

---
+++ src/cylinder.rs
@@ -0,0 +1,250 @@
+
+// This is based on a blog post found here: http://apparat-engine.blogspot.com/2013/04/procdural-meshes-cylinder.html.
+
+use std::slice::Iter;
+use bevy::math::Vec3;
+use bevy::render::mesh::{Indices, Mesh, VertexAttributeValues};
+use bevy::render::render_resource::PrimitiveTopology;
+
+pub struct Cylinder {
+    pub height: f32,
+    pub radius_bottom: f32,
+    pub radius_top: f32,
+    pub subdivisions: u32,
+}
+
+impl Default for Cylinder {
+    fn default() -> Self {
+        Self {
+            height: 1.0,
+            radius_bottom: 0.5,
+            radius_top: 0.5,
+            subdivisions: 32,
+        }
+    }
+}
+
+impl Cylinder {
+
+    /// Create a cylinder where the top and bottom disc have the same radius.
+    pub fn new_regular(height: f32, radius: f32, subdivisions: u32) -> Self {
+        Self {
+            height,
+            radius_bottom: radius,
+            radius_top: radius,
+            subdivisions
+        }
+    }
+}
+
+enum VertexPass {
+    Top, Bottom, TopRing, BottomRing,
+}
+
+impl VertexPass {
+    pub fn iter() -> Iter<'static, VertexPass> {
+        static VALUES: [VertexPass; 4] = [VertexPass::Top, VertexPass::Bottom, VertexPass::TopRing, VertexPass::BottomRing]; // order is important, top vertices come first
+        VALUES.iter()
+    }
+}
+
+/*
+impl From<Cylinder> for Mesh {
+    fn from(_: Cylinder) -> Self {
+
+        let mut vertices = Vec::new();
+        let mut indices = Vec::new();
+        let mut normals = Vec::new();
+        let mut uvs = Vec::new();
+
+        let angle_step = 2.0 * std::f32::consts::PI / 9.0;
+
+        for i in 0..10 {
+            let theta = angle_step * i as f32;
+            let position = Vec3::new(
+                0.5 * f32::cos(theta),
+                1.0,
+                0.5 * f32::sin(theta),
+            );
+            vertices.push(position.into());
+        }
+
+        for _ in 0..11 {
+            normals.push([0.0, 1.0, 0.0]);
+        }
+        for _ in 0..11 {
+            uvs.push([0.0, 0.0]);
+        }
+
+        vertices.push(Vec3::new(0.0, 1.0, 0.0).into()); // top
+
+        for i in 0..10 {
+            let l = i + 1;
+            let r = i;
+
+            indices.push((vertices.len() - 1) as u32);
+            indices.push(l);
+            indices.push(r);
+        }
+
+        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        mesh.set_indices(Some(Indices::U32(indices)));
+        mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, VertexAttributeValues::Float32x3(vertices));
+        mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, VertexAttributeValues::Float32x3(normals));
+        mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, VertexAttributeValues::Float32x2(uvs));
+        mesh
+    }
+}
+
+ */
+
+fn add_indices_top(indices: &mut Vec<u32>, mid_idx: u32, cylinder: &Cylinder) {
+
+    for i in 0..cylinder.subdivisions - 1  {
+
+        let lt = i;
+        let rt = i + 1;
+
+        indices.push(mid_idx);
+        indices.push(rt);
+        indices.push(lt);
+    }
+
+    // Fix gap where the last vertex meets the first
+    indices.push(mid_idx);
+    indices.push(0);
+    indices.push(cylinder.subdivisions - 1);
+}
+
+fn add_indices_bottom(indices: &mut Vec<u32>, mid_idx: u32, cylinder: &Cylinder) {
+
+    let base_index_bottom = cylinder.subdivisions;
+
+    for i in 0..cylinder.subdivisions - 1 {
+
+        let lb = i + base_index_bottom;
+        let rb = i + 1 + base_index_bottom;
+
+        indices.push(lb);
+        indices.push(rb);
+        indices.push(mid_idx);
+    }
+
+    // Fix gap where the last vertex meets the first
+    indices.push(base_index_bottom + cylinder.subdivisions - 1);
+    indices.push(base_index_bottom);
+    indices.push(mid_idx);
+}
+
+fn add_indices_body(indices: &mut Vec<u32>, cylinder: &Cylinder) {
+
+    let base_index_top_ring = cylinder.subdivisions * 2;
+    let base_index_bottom_ring = cylinder.subdivisions * 3;
+
+    for i in 0..cylinder.subdivisions - 1 {
+
+        let lt = i +     base_index_top_ring;
+        let rt = i + 1 + base_index_top_ring;
+        let lb = i +     base_index_bottom_ring;
+        let rb = i + 1 + base_index_bottom_ring;
+
+        indices.push(lt);
+        indices.push(rt);
+        indices.push(lb);
+        indices.push(rt);
+        indices.push(rb);
+        indices.push(lb);
+    }
+
+    // Fix gap where the last vertex meets the first
+    let lt = base_index_top_ring + cylinder.subdivisions - 1;
+    let rt = base_index_top_ring;
+    let lb = base_index_bottom_ring + cylinder.subdivisions - 1;
+    let rb = base_index_bottom_ring;
+
+    indices.push(lt);
+    indices.push(rt);
+    indices.push(lb);
+    indices.push(rt);
+    indices.push(rb);
+    indices.push(lb);
+}
+
+impl From<Cylinder> for Mesh {
+    fn from(cylinder: Cylinder) -> Self {
+
+        // Vertex order in the buffer:
+        // 1: n_subdivisions top face
+        // 2: n_subdivisions bottom face
+        // 3: n_subdivisions top outer ring
+        // 4: n_subdivisions bottom outer ring
+        // 5: top mid vertex
+        // 6: bottom mid vertex
+
+        let num_vertices = cylinder.subdivisions * 4 + 2;
+        let num_indices = cylinder.subdivisions * 2 * 6;
+        let angle_step = 2.0 * std::f32::consts::PI / cylinder.subdivisions as f32;
+
+        let mut vertices = Vec::with_capacity(num_vertices as usize);
+        let mut uvs : Vec<[f32; 2]> = Vec::with_capacity(num_vertices as usize);
+        let mut normals = Vec::with_capacity(num_vertices as usize);
+        let mut indices = Vec::with_capacity(num_indices as usize);
+
+        // Create all vertices
+
+        // Ring vertices
+        for pass in VertexPass::iter() {
+            for row_idx in 0..cylinder.subdivisions {
+
+                let theta = angle_step * row_idx as f32;
+
+                let height = match pass {
+                    VertexPass::Top => cylinder.height,
+                    VertexPass::Bottom => 0.0,
+                    VertexPass::TopRing => cylinder.height,
+                    VertexPass::BottomRing => 0.0,
+                };
+
+                let position = Vec3::new(
+                    cylinder.radius_top * f32::cos(theta),
+                    height,
+                    cylinder.radius_top * f32::sin(theta),
+                );
+
+                let normal = match pass {
+                    VertexPass::Top => Vec3::new(0.0, 1.0, 0.0),
+                    VertexPass::Bottom => Vec3::new(0.0, -1.0, 0.0),
+                    VertexPass::TopRing => Vec3::new(position.x, 0.0, position.z).normalize(),
+                    VertexPass::BottomRing => Vec3::new(position.x, 0.0, position.z).normalize(),
+                };
+
+                vertices.push(position.to_array());
+                normals.push(normal.to_array());
+                uvs.push([0.0, 0.0]);
+                // TODO: UVs
+            }
+        }
+
+        // Ring center vertices
+        vertices.push(Vec3::new(0.0, cylinder.height, 0.0).to_array()); // top
+        normals.push([0.0, 1.0, 0.0]);
+        uvs.push([0.0, 0.0]);
+        vertices.push([0.0, 0.0, 0.0]); // bottom
+        normals.push([0.0, -1.0, 0.0]);
+        uvs.push([0.0, 0.0]);
+
+        // Add the indices
+        let top_mid_idx = (vertices.len() - 2) as u32;
+        let bottom_mid_idx = (vertices.len() - 1) as u32;
+        add_indices_top(&mut indices, top_mid_idx, &cylinder);
+        add_indices_bottom(&mut indices, bottom_mid_idx, &cylinder);
+        add_indices_body(&mut indices, &cylinder);
+
+        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
+        mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+        mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+        mesh.set_indices(Some(Indices::U32(indices)));
+        mesh
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
 pub mod cone;
+pub mod cylinder;
+
 pub use crate::cone::Cone;
+pub use crate::cylinder::Cylinder;
