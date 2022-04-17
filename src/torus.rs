842addcc5c24356872e3a7d245d32bfed20638e4
Leon Suchy <lafares.lae@gmail.com>
2022-04-17 15:53:37 +0200

    Implemented indexing

--- src/torus.rs
+++ src/torus.rs
@@ -1,6 +1,7 @@
 use bevy::math::Vec3;
 use bevy::prelude::Mesh;
 use bevy::render::mesh::{Indices, PrimitiveTopology};
+use crate::util::FlatTrapezeIndices;
 
 pub struct Torus {
     /// The radius to the ring from the center
@@ -70,7 +71,39 @@ impl From<Torus> for Mesh {
             }
         }
 
+        // Lambda function that adds the indices for the faces between two vertical rings
+        let generate_vertical_ring_indices = |ring0_base_idx, ring1_base_idx| {
+            for vertical_idx in 0..(torus.vertical_segments - 1) {
+                let face = FlatTrapezeIndices {
+                    lower_left: ring0_base_idx + vertical_idx,
+                    upper_left: ring0_base_idx + vertical_idx + 1,
+                    lower_right: ring1_base_idx + vertical_idx,
+                    upper_right: ring1_base_idx + vertical_idx + 1,
+                };
+                face.generate_triangles(&mut indices);
+            }
+
+            // Stitch together the last vertices and the first
+            let face = FlatTrapezeIndices {
+                lower_left: ring0_base_idx + torus.vertical_segments - 1,
+                upper_left: ring0_base_idx,
+                lower_right: ring1_base_idx + torus.vertical_segments - 1,
+                upper_right: ring1_base_idx,
+            };
+            face.generate_triangles(&mut indices);
+        };
+
         // Add indices for each face
+        for horizontal_idx in 0..(torus.horizontal_segments - 1) {
+            let ring0_base_idx = horizontal_idx * torus.vertical_segments;
+            let ring1_base_idx = (horizontal_idx + 1) * torus.vertical_segments;
+            generate_vertical_ring_indices(ring0_base_idx, ring1_base_idx);
+        }
+
+        // Stitch together the last and first vertical ring
+        let ring0_base_idx = (torus.horizontal_segments - 1) * torus.vertical_segments;
+        let ring1_base_idx = 0;
+        generate_vertical_ring_indices(ring0_base_idx, ring1_base_idx);
 
         let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
         mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
